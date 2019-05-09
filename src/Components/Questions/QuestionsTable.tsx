15f8579ebac7e9eba22b9a960d9272693c1a9f81
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-05-09 11:10:56 +0530

    dquestion details page with title and content

--- src/Components/Questions/QuestionsTable.tsx
+++ src/Components/Questions/QuestionsTable.tsx
@@ -41,17 +41,19 @@ const QuestionsTable = ({ data }: Props) => {
                             <h2><b>{answers.length}</b></h2>
                         </td>
                         <td>
-                            <div>
-                            <h4>
-                                <Link to={'/question/' + _id} style={{ textDecoration: 'none' }}>
-                                    {title}
-                                </Link>
-                            </h4>
-                            <div className='user pull right'>
-                            <h6>{'Last Updated on: ' + new Date(lastUpdatedAt || 0).toString()}</h6>
+                            <div className="row" style={{ marginLeft: '0px', marginRight: '0px' }}>
+                                <div className="col-md-8">
+                                    <h4>
+                                        <Link to={'/question/' + _id} style={{ textDecoration: 'none' }}>
+                                            {title}
+                                        </Link>
+                                    </h4>
+                                </div>
+                                <div className="col-md-3 form-group">
+                                    <h6>{'Created on: ' + new Intl.DateTimeFormat('en-US').format(new Date(createdAt)).toString()}</h6>
+                                    {lastUpdatedAt && (<h6>{'Last Updated on: ' + new Intl.DateTimeFormat('en-US').format(new Date(lastUpdatedAt)).toString()}</h6>)}
+                                </div>
                             </div>
-                            </div>
-
                         </td>
                     </tr>
                 ))
